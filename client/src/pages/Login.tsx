135203a48d2fea249a1cd73c2ab7e20585b87c62
Pengpa0012 <peng.ilikha@gmail.com>
2023-03-06 23:59:08 +0800

    add card

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextField } from '@mui/material'
+import { Card, TextField } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
 import { useNavigate } from 'react-router-dom'
@@ -8,19 +8,21 @@ export const Login = () => {
   const navigate = useNavigate()
   return (
     <div className="min-h-screen grid place-items-center">
-      <Grid container direction="column" alignContent="center" spacing={2}>
-        <h1 className="text-center text-2xl">Product Management</h1>
-        <Grid item>
-          <TextField id="outlined-basic" label="Username" variant="outlined" style={{ width: 300 }} />
+      <Card className="px-8 py-12">
+        <Grid container direction="column" alignContent="center" spacing={2}>
+          <h1 className="text-center text-2xl pt-4">Product Management</h1>
+          <Grid item>
+            <TextField id="outlined-basic" label="Username" variant="outlined" style={{ width: 300 }} />
+          </Grid>
+          <Grid item>
+            <TextField id="outlined-basic" label="Password" variant="outlined" style={{ width: 300 }} />
+          </Grid>
+          <Grid item className="flex justify-center">
+            <Button className="flex-1" onClick={() => navigate("/register")}>Register</Button>
+            <Button variant="contained" color="success" className="!mr-2 flex-1">Login</Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <TextField id="outlined-basic" label="Password" variant="outlined" style={{ width: 300 }} />
-        </Grid>
-        <Grid item className="flex justify-center">
-          <Button className="flex-1" onClick={() => navigate("/register")}>Register</Button>
-          <Button variant="contained" color="success" className="!mr-2 flex-1">Login</Button>
-        </Grid>
-      </Grid>
+      </Card>
     </div>
   )
 }
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextField } from '@mui/material'
+import { Card, TextField } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
 import { useNavigate } from 'react-router-dom'
@@ -8,22 +8,24 @@ export const Register = () => {
   const navigate = useNavigate()
   return (
     <div className="min-h-screen grid place-items-center">
-      <Grid container direction="column" alignContent="center" spacing={2}>
-        <h1 className="text-center text-2xl">Create Account</h1>
-        <Grid item>
-          <TextField id="outlined-basic" label="Username" variant="outlined" style={{ width: 300 }} />
+      <Card className="px-8 py-12">
+        <Grid container direction="column" alignContent="center" spacing={2}>
+          <h1 className="text-center text-2xl pt-4">Create Account</h1>
+          <Grid item>
+            <TextField id="outlined-basic" label="Username" variant="outlined" style={{ width: 300 }} />
+          </Grid>
+          <Grid item>
+            <TextField id="outlined-basic" label="Password" variant="outlined" style={{ width: 300 }} />
+          </Grid>
+          <Grid item>
+            <TextField id="outlined-basic" label="Repeat Password" variant="outlined" style={{ width: 300 }} />
+          </Grid>
+          <Grid item className="flex justify-center">
+            <Button className="flex-1" onClick={() => navigate("/login")}>Login</Button>
+            <Button variant="contained" color="success" className="!mr-2 flex-1">Register</Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <TextField id="outlined-basic" label="Password" variant="outlined" style={{ width: 300 }} />
-        </Grid>
-        <Grid item>
-          <TextField id="outlined-basic" label="Repeat Password" variant="outlined" style={{ width: 300 }} />
-        </Grid>
-        <Grid item className="flex justify-center">
-          <Button className="flex-1" onClick={() => navigate("/login")}>Login</Button>
-          <Button variant="contained" color="success" className="!mr-2 flex-1">Register</Button>
-        </Grid>
-      </Grid>
+      </Card>
     </div>
   )
 }
